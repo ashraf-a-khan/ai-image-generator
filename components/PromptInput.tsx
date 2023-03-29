b757b43bb774bdc1e444d1c20cff96d30319cf54
ashraf-a-khan <khan.ashraf2311@gmail.com>
2023-03-29 00:32:28 -0400

    Complete API endpoints

--- components/PromptInput.tsx
+++ components/PromptInput.tsx
@@ -16,13 +16,21 @@ function PromptInput() {
         revalidateOnFocus: false,
     });
 
+    console.log(suggestion);
+
+    const loading = isLoading || isValidating;
+
     return (
         <div className="m-10">
             <form className="flex flex-col lg:flex-row shadow-md shadow-slate-400/10 border rounded-md lg:divide-x">
                 <textarea
                     value={input}
                     onChange={(e) => setInput(e.target.value)}
-                    placeholder="Enter a prompt"
+                    placeholder={
+                        (loading && "ChatGPT is thinking of a suggestion") ||
+                        suggestion ||
+                        "Enter a prompt..."
+                    }
                     className="flex-1 p-4 outline-none rounded-md"
                 />
                 <button
@@ -45,10 +53,20 @@ function PromptInput() {
                 <button
                     className={`p-4 bg-white text-violet-500 border-none transition-colors duration-200 rounded-b-md md:rounded-r-md md:rounded-bl-none font-bold`}
                     type="button"
+                    onClick={mutate}
                 >
                     New Suggestion
                 </button>
             </form>
+
+            {input && (
+                <p className="italic pt-2 pl-2 font-light">
+                    Suggestion:{" "}
+                    <span className="text-violet-500">
+                        {loading ? "ChatGPT is thinking..." : suggestion}
+                    </span>
+                </p>
+            )}
         </div>
     );
 }
